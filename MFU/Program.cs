01b900f02987dc1fe3def0718cab939351b0dc38
VladislavOnishchenko <vladionishchenko@gmail.com>
2020-10-18 18:48:08 +0300

    MFU Fork V - Refactor to constructor injection

--- MFU/Program.cs
+++ MFU/Program.cs
@@ -25,14 +25,36 @@ namespace MFU
         }
     }
 
+    //class MFU
+    //{
+    //    public void Print(string txt, Printer printer)
+    //    {
+    //        printer.Print(txt);
+    //    }
+
+    //    public void Scan(Scaner scaner)
+    //    {
+    //        scaner.Scan();
+    //    }
+    //}
+
     class MFU
     {
-        public void Print(string txt, Printer printer)
+        private Printer printer;
+        private Scaner scaner;
+
+        public MFU(Printer printer, Scaner scaner)
+        {
+            this.printer = printer;
+            this.scaner = scaner;
+        }
+
+        public void Print(string txt)
         {
             printer.Print(txt);
         }
 
-        public void Scan(Scaner scaner)
+        public void Scan()
         {
             scaner.Scan();
         }
@@ -43,18 +65,21 @@ namespace MFU
         static void Main(string[] args)
         {
             //использование класса "Printer"
-            Printer p = new Printer();
-            p.Print("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.");
+            //Printer p = new Printer();
+            //p.Print("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.");
             //Console.ReadKey();
 
             //использование класса "Scaner"
-            Scaner s = new Scaner();
-            s.Scan();
+            //Scaner s = new Scaner();
+            //s.Scan();
 
             //использование класса "MFU"
-            MFU m = new MFU();
-            m.Print("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.", new Printer());
-            m.Scan(new Scaner());
+            MFU m = new MFU(new Printer(), new Scaner());
+
+            //m.Print("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.", new Printer());
+            //m.Scan(new Scaner());
+            m.Print("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.");
+            m.Scan();
         }
     }
 }
